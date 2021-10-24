0fff18366080989b6454f219b02dc1b492a6cf31
Rubal Kaur <84761627+rubalkaur1903@users.noreply.github.com>
2021-10-24 15:48:37 -0700

    the front-end initial commit

---
+++ src/components/util/index.js
@@ -0,0 +1,23 @@
+const { }
+
+export const callApi = async ({ url, method, token, body }) => {
+    const history = useHistory();
+    try {
+        const options = {
+            method: method ? method.toUpperCase() : 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body)
+        };
+        if (token) options.headers['Authorization'] = `Bearer ${token}`;
+        const response = await fetch(`${REACT_APP_BASE_URL}${url}`, options);
+        const data = await response.json();
+        if(data.error) {
+             (data.error);
+        }
+        return data;
+    } catch (error) {
+        console.error(error)
+    }
+}
