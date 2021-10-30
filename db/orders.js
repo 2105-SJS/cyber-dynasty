789f2bf2cfff817c43e328050cd692e3c68ca4c1
Xavier Loera Flores <x.loera.tech@gmail.com>
2021-10-30 11:29:11 -0700

    finished getting all orders

--- db/orders.js
+++ db/orders.js
@@ -1,7 +1,53 @@
 const {client} = require('./client')
 
 
+//TODO: add products to order
+const getOrderById=(id)=>{
+try{
+        if(!id)throw Error('missing order id')
+        const {rows:[order]}=client.query(`
+        SELECT * FROM orders
+        WHERE id=$1`,[id])
+        return order;
 
-const getOrderById=(id)=>[
-    
-]
+    }catch(error){
+        throw error
+    }
+}
+
+const getAllOrders = () =>{
+    try{
+
+        const {rows:orders}=client.query(`SELECT * FROM orders`)
+
+        return orders
+    }catch(error){
+        throw error
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+    getOrderById,
+
+
+
+
+
+}
