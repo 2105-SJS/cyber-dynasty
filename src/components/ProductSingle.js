3c33953ce86fca30e98817c98d434bcace9662ca
Xavier Loera Flores <x.loera.tech@gmail.com>
2021-11-06 16:34:06 -0700

    added the grid

--- src/components/ProductSingle.js
+++ src/components/ProductSingle.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {Card, Typography} from '@material-ui/core'
 import Image from 'material-ui-image'
+import { Link } from 'react-router-dom'
 
 const ProductSingle = ({ product, children }) => {
     return (product ? 
@@ -11,9 +12,7 @@ const ProductSingle = ({ product, children }) => {
             <Typography><b>Retail Price: </b>{product.retailPrice}</Typography>
             <Typography><b>inStock? </b>{product.inStock ? 'true' : 'false'}</Typography>
             <Image src={product.thumbnail} />
-            {
-                children
-            }
+            <Link to={`/products/${product.id}`}  >Details</Link>
         </Card>
         : 'Loading......')
 }
