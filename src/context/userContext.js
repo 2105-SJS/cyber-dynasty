966a024ddf6c3c2b06b761ac6cc0af23660c5f53
Xavier Loera Flores <x.loera.tech@gmail.com>
2021-11-10 21:35:29 -0800

    started using somme cart logic

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -1,4 +1,5 @@
 import React,{createContext, useState, useEffect} from "react";
+import { callApi } from "../components/util";
 
 export const UserContext = createContext()
 
@@ -6,6 +7,36 @@ export const UserContext = createContext()
 export const UserProvider = ({children})=>{
     const [token, setToken] = useState('')
     const [isLoggedIn, setIsLoggedIn] = useState(false)
+    const [cart, setCart] = useState()
+    const [cartItems, setCartItems] = useState([]);
+
+    
+    useEffect(async () => {
+        try {
+            const user = await callApi( {
+                url: '/users/me',
+                token
+            });
+            if (user.id){
+            const cartResp = await callApi({
+                method:'GET',
+                url:'orders/cart',
+                token,
+
+            })
+            if (!cartResp.message){
+                setCart(cartResp.id)
+                if(cartResp.products && cartResp.products.length){
+                    setCartItems(cartResp.products)
+                }
+            }
+
+            }
+        } catch (error) {
+            console.error(error)
+        }
+    }, [token])  
+
     useEffect(() => {
         try {
             if(localStorage.getItem('token') != '') {
